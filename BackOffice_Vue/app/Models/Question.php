a037693282748e27f9ae658c2bfcc49903ac8592
ACD <tangngocdiep@gmail.com>
2018-11-26 16:32:49 +0700

    Relaytionship

--- BackOffice_Vue/app/Models/Question.php
+++ BackOffice_Vue/app/Models/Question.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+    function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- BackOffice_Vue/app/Models/Reply.php
+++ BackOffice_Vue/app/Models/Reply.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    function likes()
+    {
+        return $this->hasMany(Likes::class);
+    }
+
 }
