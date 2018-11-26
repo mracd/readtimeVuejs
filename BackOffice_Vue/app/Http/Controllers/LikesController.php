879154c6ba528000c11e7c8d54fda79b75bf5382
ACD <tangngocdiep@gmail.com>
2018-11-26 23:27:18 +0700

    CURD LIKE

--- BackOffice_Vue/app/Http/Controllers/LikesController.php
+++ BackOffice_Vue/app/Http/Controllers/LikesController.php
@@ -3,83 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Models\Likes;
+use App\Models\Reply;
 use Illuminate\Http\Request;
 
 class LikesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+   function likeIt(Reply $reply)
+   {
+       $reply->likes()->create([
+//           'user_id' => auth()->id()
+            'user_id'   => 1
+       ]);
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+   }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+   function unlikeIt(Reply $reply)
+   {
+//       $reply->likes()->where('user_id',auth()->id()) ->first()->delete();
+       $reply->likes()->where('user_id',1)->first()->delete();
+   }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Likes  $likes
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Likes $likes)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Likes  $likes
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Likes $likes)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Likes  $likes
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Likes $likes)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Likes  $likes
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Likes $likes)
-    {
-        //
-    }
 }
