2f6f04ab5e49c2e0d25c6092df3529146112acfb
ACD <tangngocdiep@gmail.com>
2018-11-28 14:45:38 +0700

    JWT config

--- BackOffice_Vue/app/Models/User.php
+++ BackOffice_Vue/app/Models/User.php
@@ -4,8 +4,9 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -31,5 +32,15 @@ class User extends Authenticatable
     {
         return $this->hasMany(Question::class);
     }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
 
 }
--- BackOffice_Vue/routes/api.php
+++ BackOffice_Vue/routes/api.php
@@ -6,3 +6,21 @@ Route::apiResource('question/{question}/reply','ReplyController');
 
 Route::post('/like/{reply}', 'LikesController@likeIt');
 Route::delete('/like/{reply}', 'LikesController@unlikeIt');
+
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
+
+
+
